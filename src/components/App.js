d244ad76dae56da972f8d57f38b9c634776f54ca
Brandon Barksdale <bbark93@gmail.com>
2023-10-05 17:30:02 -0400

    task 2: Create and connect an action creator

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,17 @@ import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
 import reducer, { initialState } from '../reducers';
-import { applyNumber } from '../actions'
+import { applyNumber, changeOperation } from '../actions'
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const handleNumberClick = (number) => {
     dispatch(applyNumber(number));
+  }
+
+  const handleOperatorClick = (operator) => {
+    dispatch(changeOperation(operator));
   }
 
   return (
@@ -56,9 +60,9 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton value={"+"} onClick={() => handleOperatorClick("+")}/>
+              <CalcButton value={"*"} onClick={() => handleOperatorClick("*")}/>
+              <CalcButton value={"-"} onClick={() => handleOperatorClick("-")}/>
             </div>
 
             <div className="row ce_button">
